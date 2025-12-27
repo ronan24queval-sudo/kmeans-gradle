3d86f61ad0d50c4e5210d9202cdeb167c083637c
Ronan3556 <ronan24queval@gmail.com>
2025-12-27 15:39:43 +0100

    Update KMeansTest.java

--- app/src/test/java/com/ronan/kmeans/KMeansTest.java
+++ app/src/test/java/com/ronan/kmeans/KMeansTest.java
@@ -2,6 +2,7 @@ package com.ronan.kmeans;
 
 import org.junit.jupiter.api.Test;
 import java.util.*;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 /**
@@ -11,7 +12,7 @@ import static org.junit.jupiter.api.Assertions.*;
  * <ul>
  *     <li>The K-Means algorithm runs without errors</li>
  *     <li>Initial centroids are correctly generated</li>
-     <li>Points are correctly assigned to the closest centroid</li>
+ *     <li>Points are correctly assigned to the closest centroid</li>
  *     <li>New centroids are correctly recomputed from clusters</li>
  * </ul>
  *
@@ -24,14 +25,14 @@ public class KMeansTest {
      */
     @Test
     public void testKMeansRuns() {
-        List<double[]> data = Arrays.asList(
+        final List<double[]> data = Arrays.asList(
                 new double[]{1.0, 2.0},
                 new double[]{2.0, 1.0},
                 new double[]{8.0, 9.0}
         );
 
-        KMeans k = new KMeans(2, 50);
-        List<double[]> centroids = k.fit(data);
+        final KMeans k = new KMeans(2, 50);
+        final List<double[]> centroids = k.fit(data);
 
         assertEquals(2, centroids.size());
     }
@@ -41,15 +42,16 @@ public class KMeansTest {
      */
     @Test
     public void testInitializeCentroids() {
-        KMeans k = new KMeans(2, 10);
+        final KMeans k = new KMeans(2, 10);
 
-        List<double[]> data = Arrays.asList(
+        final List<double[]> data = Arrays.asList(
                 new double[]{1, 2},
                 new double[]{3, 4},
                 new double[]{5, 6}
         );
 
-        List<double[]> centroids = k.initializeCentroids(new ArrayList<>(data));
+        final List<double[]> centroids =
+                k.initializeCentroids(new ArrayList<>(data));
 
         assertEquals(2, centroids.size(), "Should pick exactly k initial centroids");
     }
@@ -59,21 +61,22 @@ public class KMeansTest {
      */
     @Test
     public void testAssignPointsToClusters() {
-        KMeans k = new KMeans(2, 10);
+        final KMeans k = new KMeans(2, 10);
 
-        List<double[]> data = Arrays.asList(
+        final List<double[]> data = Arrays.asList(
                 new double[]{1, 1},
                 new double[]{2, 2},
                 new double[]{8, 8},
                 new double[]{9, 9}
         );
 
-        List<double[]> centroids = Arrays.asList(
+        final List<double[]> centroids = Arrays.asList(
                 new double[]{1, 1},
                 new double[]{9, 9}
         );
 
-        Map<double[], List<double[]>> clusters = k.assignPointsToClusters(data, centroids);
+        final Map<double[], List<double[]>> clusters =
+                k.assignPointsToClusters(data, centroids);
 
         assertEquals(2, clusters.size());
         assertEquals(2, clusters.get(centroids.get(0)).size());
@@ -85,21 +88,27 @@ public class KMeansTest {
      */
     @Test
     public void testRecomputeCentroids() {
-        KMeans k = new KMeans(2, 10);
+        final KMeans k = new KMeans(2, 10);
 
-        Map<double[], List<double[]>> clusters = new HashMap<>();
+        final Map<double[], List<double[]>> clusters = new HashMap<>();
 
-        clusters.put(new double[]{0, 0}, Arrays.asList(
-                new double[]{1, 1},
-                new double[]{3, 3}
-        ));
+        clusters.put(
+                new double[]{0, 0},
+                Arrays.asList(
+                        new double[]{1, 1},
+                        new double[]{3, 3}
+                )
+        );
 
-        clusters.put(new double[]{0, 0}, Arrays.asList(
-                new double[]{10, 10},
-                new double[]{14, 14}
-        ));
+        clusters.put(
+                new double[]{0, 0},
+                Arrays.asList(
+                        new double[]{10, 10},
+                        new double[]{14, 14}
+                )
+        );
 
-        List<double[]> newCentroids = k.recomputeCentroids(clusters);
+        final List<double[]> newCentroids = k.recomputeCentroids(clusters);
 
         assertArrayEquals(new double[]{2.0, 2.0}, newCentroids.get(0));
         assertArrayEquals(new double[]{12.0, 12.0}, newCentroids.get(1));
