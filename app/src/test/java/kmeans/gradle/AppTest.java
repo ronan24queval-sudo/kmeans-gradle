db4fb6744e2619e3bd7f53d4bb796a1b554648a5
Ronan3556 <ronan24queval@gmail.com>
2025-12-27 15:42:20 +0100

    Update AppTest.java

--- app/src/test/java/kmeans/gradle/AppTest.java
+++ app/src/test/java/kmeans/gradle/AppTest.java
@@ -4,11 +4,14 @@
 package kmeans.gradle;
 
 import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class AppTest {
-    @Test void appHasAGreeting() {
-        App classUnderTest = new App();
+
+    @Test
+    void appHasAGreeting() {
+        final App classUnderTest = new App();
         assertNotNull(classUnderTest.getGreeting(), "app should have a greeting");
     }
 }
